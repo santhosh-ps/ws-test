93f71ace73d5963e2401388968e97cd504add7eb
Santhosh ps <pssanthosh13@gmail.com>
2018-04-24 17:22:37 +0530

    init test

---
+++ main.js
@@ -0,0 +1,14 @@
+//@ts-check
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 8080
+});
+let id = 0;
+wss.on('connection',function(ws) {
+  ws.id=id++;
+  console.log("User connected", ws.id);
+  ws.on('close',function(data) {
+  console.log("closed connection :",ws.id);    
+  })
+})
